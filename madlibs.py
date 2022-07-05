580021f81ec6360884690a90e2dfe9fb4a310db3
jxiao21 <94207497+jxiao21@users.noreply.github.com>
2022-07-05 13:24:21 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+plural_nouns = input("Plural noun: ")
+pluralnoun = input("Plural noun: ")
+number = input("Number: ")
+noun = input("Noun: ")
+verb = input("Verb: ")
+adjective = input("Adjective: ")
+number2 = input("Number greater than 1: ")
+adjective2 = input("Adjective: ")
+noun2 = input("Noun: ")
+adjective3 = input("Adjective: ")
+print(f"Hello world! Today we see the ocean {plural_nouns} \
+    and all that is in it. These {pluralnoun} are a result of \
+    the waves washing them out to the shore for {number} million years! \
+    That's almost as old as {noun}! Anyways, we are going to \
+    {verb} along this {adjective} shore for {number2} hours as \
+    we admire the {adjective2} morning {noun2} on this {adjective3} day!")
